b1500ecb4b25a9d081aa6b88593f5261b870bc7d
Wpenga <1687219868@qq.com>
2021-09-16 16:56:58 +0800

    添加cookie

---
+++ dev.go
@@ -0,0 +1,11 @@
+package main
+import (
+      //青龙2.9机器人
+      _ "github.com/cdle/sillyGirl/develop/qinglong"
+      //资产查询
+      _ "github.com/cdle/sillyGirl/develop/jd_asset"   
+      //京东账号
+      //_ "github.com/ufuckee/jd_cookie"
+      _ "github.com/cdle/sillyGirl/im/qq"
+      _ "github.com/cdle/sillyGirl/im/tg"
+)
