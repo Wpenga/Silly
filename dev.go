e96e27a570c855baf069d38a57abd45eefb31046
Wpeng <72253291+Wpenga@users.noreply.github.com>
2021-09-16 18:01:36 +0800

    Update dev.go

--- dev.go
+++ dev.go
@@ -3,7 +3,7 @@ import (
       //青龙2.9机器人
       _ "github.com/cdle/sillyGirl/develop/qinglong"
       //资产查询
-      _ "github.com/cdle/sillyGirl/develop/jd_asset"   
+     // _ "github.com/cdle/sillyGirl/develop/jd_asset"   
       //京东账号
       _ "github.com/ufuckee/jd_cookie"
       _ "github.com/cdle/sillyGirl/im/qq"
