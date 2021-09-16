244ef29394490b9abc39bdd33b7ae1e014e51e0a
Wpeng <72253291+Wpenga@users.noreply.github.com>
2021-09-16 18:01:15 +0800

    Update dev.go

--- dev.go
+++ dev.go
@@ -5,7 +5,7 @@ import (
       //资产查询
       _ "github.com/cdle/sillyGirl/develop/jd_asset"   
       //京东账号
-      //_ "github.com/ufuckee/jd_cookie"
+      _ "github.com/ufuckee/jd_cookie"
       _ "github.com/cdle/sillyGirl/im/qq"
       _ "github.com/cdle/sillyGirl/im/tg"
 )
